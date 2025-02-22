5ca1863b5247e2453c0fe52c97970da9c71e4de4
devin-sachs <75508790+devin-sachs@users.noreply.github.com>
2025-02-21 22:53:03 -0800

    Refactor library code and add basic read/remove

--- script.js
+++ script.js
@@ -1,77 +1,96 @@
 const myLibrary = [];
 
 function Book(title, author, pages) {
-  // the constructor...
   this.title = title;
   this.author = author;
   this.pages = pages;
 }
 
 function addBookToLibrary(title, author, pages){
-    // take params, create a book then store it in the array
-
-    currentBook = new Book(title,author,pages);
-    myLibrary.push(currentBook);
+    const newBook = new Book(title,author,pages);
+    myLibrary.push(newBook);
+    displayLibrary(newBook);
 }
 
-function displayLibrary(){
-    myLibrary.forEach(book => {
-        console.log(book)
+function displayLibrary(book){
+    if (!book) return; // Avoid errors if no book is passed
 
-        const card_container = document.querySelector(".card-container")
-        const card = document.createElement("div");
-        card.classList.add("card")
-        card_container.appendChild(card);
+    const card_container = document.querySelector(".card-container");
     
-        const book_details = document.createElement("div");
-        book_details.classList.add("book-details");
-        card.appendChild(book_details);
-    
-                const list = document.createElement("ul")
-    
-        for (let info in book){
-    
-            const list_item = document.createElement("li")
-            list.appendChild(list_item);
-            // console.log((info + ": "+ book[info]))
-    
-            firstLetter  = info.charAt(0);
-            firstLetterCap  = firstLetter .toUpperCase();
-            remainingLetters  = info.slice(1);
-            capitalizedProp = firstLetterCap + remainingLetters;
-    
-            // console.log(capitalizedProp)
-    
-    
-            book_text = document.createTextNode(capitalizedProp + ": " + book[info]);
-            list_item.appendChild(book_text);
-            book_details.appendChild(list);
-        }
+    const card = document.createElement("div");
+    card.classList.add("card");
+    card_container.appendChild(card);
+
+    const book_details = document.createElement("div");
+    book_details.classList.add("book-details");
+    card.appendChild(book_details);
+
+    // const list = document.createElement("ul");
+
+    for (let info in book){
+
+        const bookProp = document.createElement("p")
+
+
+        // Capitalize the property name
+        firstLetter  = info.charAt(0);
+        firstLetterCap  = firstLetter .toUpperCase();
+        remainingLetters  = info.slice(1);
+        capitalizedProp = firstLetterCap + remainingLetters;    
         
-    })
+        // Create text node
+        book_text = document.createTextNode(capitalizedProp + ": " + book[info]);
+        bookProp.appendChild(book_text);
+        book_details.appendChild(bookProp);
+
+    }
+
+            //Create bottom card buttons
+            const buttonContainer = document.createElement("div");
+            buttonContainer.classList.add("button-card-container");
+            
+            const readButton = document.createElement("button");
+            readButton.classList.add("read");
+            readButtonText = document.createTextNode("Read")
+            readButton.appendChild(readButtonText);
+    
+            const deleteButton = document.createElement("button");
+            deleteButton.classList.add("delete")
+            deleteButtonText = document.createTextNode("Remove Book");
+            deleteButton.appendChild(deleteButtonText);
+    
+            buttonContainer.appendChild(readButton);
+            buttonContainer.appendChild(deleteButton);
+    
+            book_details.appendChild(buttonContainer);
+
+    // book_details.
 }
 
-addBookToLibrary("Fight Club", "Chuck Palahniuk", "208");
-addBookToLibrary("Normal People", "Sally Rooney", "266");
-addBookToLibrary("Haunting Adeline", "H.D. Carlton", "565");
-addBookToLibrary("Hunting Adeline", "H.D. Carlton", "702");
-addBookToLibrary("My Best Friend's Exorcism", "Grady Hendrix", "336");
 
-
+// // Handle form submission
 const dialog = document.querySelector("dialog");
 const showButton = document.querySelector(".add-book");
-const closeButton = document.querySelector("dialog button");
+const submitButton = document.querySelector("dialog button");
+const formInputs = []
 
 // "Show the dialog" button opens the dialog modally
 showButton.addEventListener("click", () => {
   dialog.showModal();
 });
 
-// "Close" button closes the dialog
-closeButton.addEventListener("click", () => {
-  dialog.close();
+submitButton.addEventListener("click", (event) => {
+    event.preventDefault();
+    const formTitle = document.querySelector("#title").value;
+    const formAuthor = document.querySelector("#author").value;
+    const formPages = document.querySelector("#pages").value;
+    addBookToLibrary(formTitle,formAuthor,formPages);
+    dialog.close();
 });
 
 
-// console.log(myLibrary);
-displayLibrary()
+addBookToLibrary("Fight Club", "Chuck Palahniuk", "208");
+addBookToLibrary("Normal People", "Sally Rooney", "266");
+addBookToLibrary("Haunting Adeline", "H.D. Carlton", "565");
+addBookToLibrary("Hunting Adeline", "H.D. Carlton", "702");
+addBookToLibrary("My Best Friend's Exorcism", "Grady Hendrix", "336");
