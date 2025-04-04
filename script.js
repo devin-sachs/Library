a818958c94839efe80b192307528bc25b1959ba1
devin-sachs <75508790+devin-sachs@users.noreply.github.com>
2025-04-04 10:25:11 -0700

    Refactor code to use Book Class and methods
    moved form processing to its own function along with the deletion of
    books in its own function as well to help organize codebase.

--- script.js
+++ script.js
@@ -1,155 +1,176 @@
+
 const myLibrary = [];
 
-function Book(title, author, pages, readStatus) {
-  this.title = title;
-  this.author = author;
-  this.pages = pages;
-  this.readStatus = readStatus
-}
-
-function addBookToLibrary(title, author, pages,readStatus){
-    const newBook = new Book(title,author,pages, readStatus);
-    myLibrary.push(newBook);
-    displayLibrary(newBook);
-}
-
-function displayLibrary(book){
-    if (!book) return; // Avoid errors if no book is passed
-
-    const card_container = document.querySelector(".card-container");
-    
-    const card = document.createElement("div");
-    card.classList.add("card");
-    card_container.appendChild(card);
-
-    const book_details = document.createElement("div");
-    book_details.classList.add("book-details");
-    card.appendChild(book_details);
-
-    for (let info in book){
-        if (info == "readStatus"){
-            break;
-        }
-
-        const bookProp = document.createElement("p")
-
-        // Capitalize the property name
-        firstLetter  = info.charAt(0);
-        firstLetterCap  = firstLetter .toUpperCase();
-        remainingLetters  = info.slice(1);
-        capitalizedProp = firstLetterCap + remainingLetters;    
-        
-        // Create text node
-        book_text = document.createTextNode(capitalizedProp + ": " + book[info]);
-        bookProp.appendChild(book_text);
-        book_details.appendChild(bookProp);
-
+class Book{
+    constructor(title, author, pages, readStatus){
+        this.title = title;
+        this.author = author;
+        this.pages = pages;
+        this.readStatus = readStatus
     }
 
-            //Create bottom card buttons
-            const buttonContainer = document.createElement("div");
-            buttonContainer.classList.add("button-card-container");
-            
-            const readButton = document.createElement("button");
-            readButton.classList.add("read");
-            readButtonText = document.createTextNode("Read")
-            readButton.appendChild(readButtonText);
+    //Book method
+    addBookToLibrary(){
+        // const newBook = new Book(this.title,this.author,this.ages, this.readStatus);
+        myLibrary.push(this);
+        this.displayLibrary(this);
+    }
 
-            if (book.readStatus != true){
-                readButton.classList.toggle("unread");
-                readButton.textContent = "Unread"
+    displayLibrary(book){
+        if (!book) return; // Avoid errors if no book is passed
+    
+        const card_container = document.querySelector(".card-container");
+        
+        const card = document.createElement("div");
+        card.classList.add("card");
+        card_container.appendChild(card);
+    
+        const book_details = document.createElement("div");
+        book_details.classList.add("book-details");
+        card.appendChild(book_details);
+    
+        for (let info in book){
+            if (info == "readStatus"){
+                break;
             }
     
-            const deleteButton = document.createElement("button");
-            deleteButton.classList.add("delete")
-            deleteButtonText = document.createTextNode("Delete");
-            deleteButton.appendChild(deleteButtonText);
+            const bookProp = document.createElement("p")
     
-            buttonContainer.appendChild(readButton);
-            buttonContainer.appendChild(deleteButton);
+            // Capitalize the property name
+            let firstLetter  = info.charAt(0);
+            let firstLetterCap  = firstLetter .toUpperCase();
+            let remainingLetters  = info.slice(1);
+            let capitalizedProp = firstLetterCap + remainingLetters;    
+            
+            // Create text node
+            let book_text = document.createTextNode(capitalizedProp + ": " + book[info]);
+            bookProp.appendChild(book_text);
+            book_details.appendChild(bookProp);
     
-            book_details.appendChild(buttonContainer);
+        }
+    
+                //Create bottom card buttons
+                const buttonContainer = document.createElement("div");
+                buttonContainer.classList.add("button-card-container");
+                
+                const readButton = document.createElement("button");
+                readButton.classList.add("read");
+                let readButtonText = document.createTextNode("Read")
+                readButton.appendChild(readButtonText);
+    
+                if (book.readStatus != true){
+                    readButton.classList.toggle("unread");
+                    readButton.textContent = "Unread"
+                }
+        
+                const deleteButton = document.createElement("button");
+                deleteButton.classList.add("delete")
+                let deleteButtonText = document.createTextNode("Delete");
+                deleteButton.appendChild(deleteButtonText);
+        
+                buttonContainer.appendChild(readButton);
+                buttonContainer.appendChild(deleteButton);
+        
+                book_details.appendChild(buttonContainer);
+    
+    }
 
 }
 
 
-// // Handle form submission
-const dialog = document.querySelector("dialog");
-const showButton = document.querySelector(".add-book");
-const submitButton = document.querySelector("dialog button");
+function processForm (){
 
-// "Show the dialog" button opens the dialog modally
-showButton.addEventListener("click", () => {
-  dialog.showModal();
-});
+    // // Handle form submission
+    const dialog = document.querySelector("dialog");
+    const showButton = document.querySelector(".add-book");
+    const submitButton = document.querySelector("dialog button");
 
-//Add Book button event listener 
-submitButton.addEventListener("click", (event) => {
-    event.preventDefault();
-    const formTitle = document.querySelector("#title").value;
-    const formAuthor = document.querySelector("#author").value;
-    const formPages = document.querySelector("#pages").value;
-    const formReadStatus = document.querySelector('#readStatus').checked;
-    // console.log(formReadStatus);
-    addBookToLibrary(formTitle,formAuthor,formPages, formReadStatus);
-    dialog.close();
-});
+    // "Show the dialog" button opens the dialog modally
+    showButton.addEventListener("click", () => {
+    dialog.showModal();
+    });
 
+    //Add Book button event listener 
+    submitButton.addEventListener("click", (event) => {
+        event.preventDefault();
+        const formTitle = document.querySelector("#title").value;
+        const formAuthor = document.querySelector("#author").value;
+        const formPages = document.querySelector("#pages").value;
+        const formReadStatus = document.querySelector('#readStatus').checked;
+        // console.log(formReadStatus);
+        currentBook = new Book(formTitle,formAuthor,formPages, formReadStatus);
+        currentBook.addBookToLibrary();
+        dialog.close();
+    });
+}
 
-addBookToLibrary("Fight Club", "Chuck Palahniuk", "208", true);
-addBookToLibrary("Normal People", "Sally Rooney", "266", true);
-addBookToLibrary("Haunting Adeline", "H.D. Carlton", "565", true);
-addBookToLibrary("Hunting Adeline", "H.D. Carlton", "702", false);
-addBookToLibrary("My Best Friend's Exorcism", "Grady Hendrix", "336", false);
-
-
+function addDeleteButton(){
 //Delete button, if pressed delete book and remove from library, matching by title. Ideally would have some kind of internal ID property if used in DB for future use. 
 cardContainer = document.querySelector(".card-container")
 
 cardContainer.addEventListener("click",(event) => {
 
-    const currentCard = event.target.closest(".card")
-    const bookDetails = currentCard.querySelector(".book-details");
-    const buttonContainer = bookDetails.querySelector(".button-card-container")
+const currentCard = event.target.closest(".card")
+const bookDetails = currentCard.querySelector(".book-details");
+const buttonContainer = bookDetails.querySelector(".button-card-container")
 
-    const readButton = buttonContainer.querySelector(".read")
+const readButton = buttonContainer.querySelector(".read")
 
-    if (event.target.matches(".delete")) {
-        const bookTitleElement = bookDetails.querySelector("p")
+if (event.target.matches(".delete")) {
+    const bookTitleElement = bookDetails.querySelector("p")
 
-        if (!bookTitleElement) return;
+    if (!bookTitleElement) return;
 
-        currentBookTitle = bookTitleElement.textContent.slice(7).trim();
+    currentBookTitle = bookTitleElement.textContent.slice(7).trim();
 
-        const bookIndex = myLibrary.findIndex(book => book.title === currentBookTitle);
+    const bookIndex = myLibrary.findIndex(book => book.title === currentBookTitle);
 
-        if (bookIndex !== -1) {
-            // console.log("removing book", myLibrary[bookIndex])
-            myLibrary.splice(bookIndex,1)
-            currentCard.remove();
-            // console.log("Updated library", myLibrary);
-        }
-        
+    if (bookIndex !== -1) {
+        // console.log("removing book", myLibrary[bookIndex])
+        myLibrary.splice(bookIndex,1)
+        currentCard.remove();
+        // console.log("Updated library", myLibrary);
     }
-
-    else if(event.target.matches(".read")) {
-        // readButton.classList.toggle(".unread");
-        readButton.classList.toggle("unread");
-        readButton.textContent = "Unread"
-
-
-        //toggle button text based on current class
-
-        //TO DO future improvement: 
-
-        //create Book prototype function that toggles a book instance’s read status. Need to add a class property for read or not read
-        if(readButton.classList.contains("unread")){
-            readButton.textContent = "Unread"
-        } else{
-            readButton.textContent = "Read"
-        }
-        // console.log(readButton);
-    } 
     
+}
+
+else if(event.target.matches(".read")) {
+    // readButton.classList.toggle(".unread");
+    readButton.classList.toggle("unread");
+    readButton.textContent = "Unread"
+
+
+    //toggle button text based on current class
+
+    //TO DO future improvement: 
+
+    //create Book prototype function that toggles a book instance’s read status. Need to add a class property for read or not read
+    if(readButton.classList.contains("unread")){
+        readButton.textContent = "Unread"
+    } else{
+        readButton.textContent = "Read"
+    }
+    // console.log(readButton);
+} 
+
 
 })
+}
+
+
+
+fight_club = new Book("Fight Club", "Chuck Palahniuk", "208", true)
+normal_people = new Book("Normal People", "Sally Rooney", "266", true)
+haunting_adeline = new Book("Haunting Adeline", "H.D. Carlton", "565", true)
+haunting_adeline = new Book("Hunting Adeline", "H.D. Carlton", "702", false)
+best_friends = new Book("My Best Friend's Exorcism", "Grady Hendrix", "336", false)
+
+fight_club.addBookToLibrary();
+normal_people.addBookToLibrary();
+haunting_adeline.addBookToLibrary();
+haunting_adeline.addBookToLibrary();
+best_friends.addBookToLibrary();
+
+
+processForm();
+addDeleteButton();
